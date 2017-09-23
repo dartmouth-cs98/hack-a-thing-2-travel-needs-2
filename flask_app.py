ad58c7f8ec5891abdeb3c4561fd6f550119cb750
Jiyun Sung <jiyunsung@jiyuns-mbp.wireless.dartmouth.edu>
2017-09-23 18:07:09 -0400

    country/destination

--- flask_app.py
+++ flask_app.py
@@ -48,9 +48,22 @@ def handle_incoming_messages():
     response_obj = json.loads(responsestr)
     if 'result' in response_obj:
         response = response_obj["result"]["fulfillment"]["speech"]
-        reply(sender, response)
+        reply(sender, get_info(response))
 
     return "ok"
+
+def get_info(response):
+    info = response.split(",")
+    if info[0] == "banana":
+        citizenship, destination, start_date, end_date = info[1], info[2], info[3], info[4]
+
+        if citizenship == "Korea" and destination == "Viet Nam":
+            response = "Traveling to " + destination + " as a citizen of " + citizenship + " does not require a visa if you're staying for less than 15 days and if you haven't visited $destination in the last 6 months.\n\n" +"Standard voltage in " + destination + " is 110/220V.\n\n"+"Average high temperature in " + destination + " from " + start_date + " to " + end_date + " is 37.9 C / 91.04 F with an average low of 26.2 C / 79.16 F. Humidity will be over 80%, and there is a high chance of rain during your stay, so bring an umbrella!\n\n"+"The currency used in " + destination +" is the Vietnamese Dong (VND). 1 SKW is currently equal to 20.05 VND."
+        elif citizenship == "China" and destination == "United Arab Emirates":
+            response = "If you're traveling to  " + destination + " as a citizen of " + citizenship + ", you will be granted a visa on arrival.\n\n"+"Standard voltage in " + destination + " is 220-240V. You will need an adaptor as the plugs look different.\n\n"+"Average high temperature in " + destination + " from " + start_date + " to " + end_date + " is 100.3 C / 100.3 F with an average low of 27.2 C / 81 F. Humidity will be around 55%, and it will be sunny, so pack some sunscreen!\n\n"+"The currency used in " + destination +" is the United Arab Emirates dirham. 1 Yuan is currently equal to 0.56 dirham."
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
